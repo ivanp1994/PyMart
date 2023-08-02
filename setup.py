3787384dfc261c493e93c349ce0b6a461955bc11
Ivan Pokrovac <ivan.pokrovac.fbf@gmail.com>
2023-08-02 10:00:15 +0200

    Fixed a typo in setup.py, changed stuff in requirements_dev, added twine and wheel

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name="PyMart",
       classifiers=["Development Status :: 3 - Alpha",
                    "License :: OSI Approved :: MIT License",
                    "Programming Language :: Python :: 3.8",
-                   "Programming Language :: Python :: 3 :: Only"
+                   "Programming Language :: Python :: 3 :: Only",
                    "Intended Audience :: Science/Research"],
       install_requires=install_reqs,
       python_requires=">=3.8",
